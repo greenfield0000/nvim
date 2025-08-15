3861ab89d667019e1e336deafeaa6eff533c7134
rivanov <rivanov@bivgroup.ru>
2025-08-15 13:32:41 +0300

    add auto install mason

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,127 +1,110 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			-- setup mason with default properties
-			require("mason").setup({
-				ui = {
-					border = "rounded",
-				},
-			})
-		end,
-	},
-	-- mason nvim dap utilizes mason to automatically ensure debug adapters you want installed are installed, mason-lspconfig will not automatically install debug adapters for us
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		config = function()
-			-- ensure the java debug adapter is installed
-			require("mason-nvim-dap").setup({
-				ensure_installed = { "java-debug-adapter", "java-test" },
-			})
-		end,
-	},
-	-- utility plugin for configuring the java language server for us
-	{
-		"mfussenegger/nvim-jdtls",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"ray-x/lsp_signature.nvim",
-		},
-	},
-	{
-		"ray-x/lsp_signature.nvim",
-		config = function()
-			require("lsp_signature").setup()
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local icons = require("config.icons")
+    -- utility plugin for configuring the java language server for us
+    {
+        "mfussenegger/nvim-jdtls",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "ray-x/lsp_signature.nvim",
+        },
+    },
+    {
+        "ray-x/lsp_signature.nvim",
+        config = function()
+            require("lsp_signature").setup()
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local icons = require("config.icons")
 
-			-- get access to the lspconfig plugins functions
-			local lspconfig = require("lspconfig")
+            -- get access to the lspconfig plugins functions
+            local lspconfig = require("lspconfig")
 
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
 
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
+            lspconfig.jq.setup({
+                capabilities = capabilities,
+            })
 
-			lspconfig.lemminx.setup({
-				capabilities = capabilities,
-			})
+            lspconfig.gopls.setup({
+                capabilities = capabilities,
+            })
 
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
+            lspconfig.lemminx.setup({
+                capabilities = capabilities,
+            })
 
-			lspconfig.yamlls.setup({
-				capabilities = capabilities,
-			})
+            lspconfig.jsonls.setup({
+                capabilities = capabilities,
+            })
 
-			lspconfig.sqlls.setup({
-				capabilities = capabilities,
-			})
+            lspconfig.yamlls.setup({
+                capabilities = capabilities,
+            })
 
-			local default_diagnostic_config = {
-				signs = {
-					active = true,
-					values = {
-						{ name = "DiagnosticSignError", text = icons.diagnostics.Error },
-						{ name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
-						{ name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
-						{ name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
-					},
-				},
-				virtual_text = false,
-				update_in_insert = false,
-				underline = true,
-				severity_sort = true,
-				float = {
-					focusable = true,
-					style = "minimal",
-					border = "rounded",
-					source = "always",
-					header = "",
-					prefix = "",
-				},
-			}
+            lspconfig.sqlls.setup({
+                capabilities = capabilities,
+            })
 
-			vim.diagnostic.config(default_diagnostic_config)
+            local default_diagnostic_config = {
+                signs = {
+                    active = true,
+                    values = {
+                        { name = "DiagnosticSignError", text = icons.diagnostics.Error },
+                        { name = "DiagnosticSignWarn",  text = icons.diagnostics.Warning },
+                        { name = "DiagnosticSignHint",  text = icons.diagnostics.Hint },
+                        { name = "DiagnosticSignInfo",  text = icons.diagnostics.Information },
+                    },
+                },
+                virtual_text = false,
+                update_in_insert = false,
+                underline = true,
+                severity_sort = true,
+                float = {
+                    focusable = true,
+                    style = "minimal",
+                    border = "rounded",
+                    source = "always",
+                    header = "",
+                    prefix = "",
+                },
+            }
 
-			for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
-				vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
-			end
+            vim.diagnostic.config(default_diagnostic_config)
 
-			-- Set vim motion for <Space> + c + h to show code documentation about the code the cursor is currently over if available
-			vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
-			-- Set vim motion for <Space> + c + d to go where the code/variable under the cursor was defined
-			vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode Goto [D]efinition" })
-			-- Set vim motion for <Space> + c + a for display code action suggestions for code diagnostics in both normal and visual mode
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
-			-- Set vim motion for <Space> + c + r to display references to the code under the cursor
-			vim.keymap.set(
-				"n",
-				"<leader>cr",
-				require("telescope.builtin").lsp_references,
-				{ desc = "[C]ode Goto [R]eferences" }
-			)
-			-- Set vim motion for <Space> + c + i to display implementations to the code under the cursor
-			vim.keymap.set(
-				"n",
-				"<leader>ci",
-				require("telescope.builtin").lsp_implementations,
-				{ desc = "[C]ode Goto [I]mplementations" }
-			)
-			-- Set a vim motion for <Space> + c + <Shift>R to smartly rename the code under the cursor
-			vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
-			-- Set a vim motion for <Space> + c + <Shift>D to go to where the code/object was declared in the project (class file)
-			vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
-		end,
-	},
+            for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
+                vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
+            end
+
+            -- Set vim motion for <Space> + c + h to show code documentation about the code the cursor is currently over if available
+            vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
+            -- Set vim motion for <Space> + c + d to go where the code/variable under the cursor was defined
+            vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode Goto [D]efinition" })
+            -- Set vim motion for <Space> + c + a for display code action suggestions for code diagnostics in both normal and visual mode
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
+            -- Set vim motion for <Space> + c + r to display references to the code under the cursor
+            vim.keymap.set(
+                "n",
+                "<leader>cr",
+                require("telescope.builtin").lsp_references,
+                { desc = "[C]ode Goto [R]eferences" }
+            )
+            -- Set vim motion for <Space> + c + i to display implementations to the code under the cursor
+            vim.keymap.set(
+                "n",
+                "<leader>ci",
+                require("telescope.builtin").lsp_implementations,
+                { desc = "[C]ode Goto [I]mplementations" }
+            )
+            -- Set a vim motion for <Space> + c + <Shift>R to smartly rename the code under the cursor
+            vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
+            -- Set a vim motion for <Space> + c + <Shift>D to go to where the code/object was declared in the project (class file)
+            vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
+        end,
+    },
 }
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,49 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        config = function()
+            require("mason-tool-installer").setup({
+                ensure_installed = {
+                    "glow",
+                    "lua-language-server",
+                    "marksman",
+                    "prettier",
+                    "python-lsp-server",
+                    "rust-analyzer",
+                    "shellcheck",
+                    "shellharden",
+                    "stylua",
+                    "zls",
+                },
+            })
+        end,
+    },
+    -- mason nvim dap utilizes mason to automatically ensure debug adapters you want installed are installed,
+    -- mason-lspconfig will not automatically install debug adapters for us
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        config = function()
+            -- ensure the java debug adapter is installed
+            require("mason-nvim-dap").setup({
+                ensure_installed = {
+                    "java-debug-adapter",
+                    "java-test",
+                },
+            })
+        end,
+    },
+}
