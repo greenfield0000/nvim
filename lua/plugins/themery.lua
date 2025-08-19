1b78442ee20b7ae2ccea55c7ec9f75b3b82ca827
rivanov <rivanov@bivgroup.ru>
2025-08-19 21:41:41 +0300

    Revert "Fix java"
    This reverts commit 89317d7d35232912fb4d2c65f543453b5489c908.

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -6,7 +6,7 @@ return {
             config = function()
                 require("themery").setup({
                     -- add the config here
-                    themes = { "jb", "dracula", "tokyonight"}, -- Your list of installed colorschemes.
+                    themes = { "jb"}, -- Your list of installed colorschemes.
                     livePreview = true,
                 })
             end
